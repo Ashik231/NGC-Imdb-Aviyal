63b89ee88a198e9cc857578b1d21f26b2577bb94
ᴀꜱʜɪᴋ ᴍʜᴅ <85797232+Ashik231@users.noreply.github.com>
2022-07-13 16:59:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -174,7 +174,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⎝⎙{get_size(file.file_size)}⎙⎞ ⎝⏍{file.file_name}⏍⎞", callback_data=f'files#{file.file_id}'
+                    text=f"⎝{get_size(file.file_size)}⎞ ⎝{file.file_name}⎞", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -183,10 +183,10 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⎝⏍{file.file_name}⏍⎞", callback_data=f'files#{file.file_id}'
+                    text=f"⎝{file.file_name}⎞", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"⎝⎙{get_size(file.file_size)}⎙⎞",
+                    text=f"⎝{get_size(file.file_size)}⎞",
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
@@ -1331,7 +1331,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⎝⎙{get_size(file.file_size)}⎙⎞ ⎝⏍{file.file_name}⏍⎞", callback_data=f'{pre}#{file.file_id}'
+                    text=f"⎝{get_size(file.file_size)}⎞ ⎝{file.file_name}⎞", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1340,11 +1340,11 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"⎝⏍{file.file_name}⏍⎞",
+                    text=f"⎝{file.file_name}⎞",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"⎝⎙{get_size(file.file_size)}⎙⎞",
+                    text=f"⎝{get_size(file.file_size)}⎞",
                     callback_data=f'{pre}_#{file.file_id}',
                 ),
             ]
