a2582da87481c059f4c126307cb340192bc33a0c
ᴀꜱʜɪᴋ ᴍʜᴅ <85797232+Ashik231@users.noreply.github.com>
2022-07-14 18:43:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/Ashik231/NGC-Imdb-Aviyal.git /NGC-Imdb-Aviyal
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /NGC-Imdb-Aviyal
 fi
 cd /NGC-Imdb-Aviyal
 pip3 install -U -r requirements.txt
