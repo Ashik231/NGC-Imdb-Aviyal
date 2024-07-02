fe947545664eaf0db8d794aa21af9c47d96bcbcb
ᴀꜱʜɪᴋ ᴍʜᴅ <85797232+Ashik231@users.noreply.github.com>
2024-07-03 00:53:27 +0530

    Update Railway_Script.py

--- Railway_Script.py
+++ Railway_Script.py
@@ -1,12 +1,1 @@
-{
-  "$schema": "https://railway.app/railway.schema.json",
-  "build": {
-    "builder": "DOCKERFILE",
-    "dockerfilePath": "Dockerfile"
-  },
-  "deploy": {
-    "startCommand": "python3 bot.py",
-    "restartPolicyType": "ON_FAILURE",
-    "restartPolicyMaxRetries": 10
-  }
-}
+
